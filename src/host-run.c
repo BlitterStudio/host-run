e3d7b425adff8a692d4f4412a20c4742235414d2
Dimitris Panokostas <midwan@gmail.com>
2020-06-26 19:37:37 +0200

    First version

---
+++ src/host-run.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "uae_pragmas.h"
+
+static const char version[] = "$VER: Host-Run v1.0";
+
+int main(int argc, char *argv[])
+{
+    char command[255];
+
+    if (argc <= 1)
+    {
+        printf("Missing argument\n");
+        return 0;
+    }
+
+    if (!InitUAEResource())
+    {
+        printf("UAEResource not found!\n");
+        return 2;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        strcat(command, argv[i]);
+
+        if (i != argc - 1)
+            strcat(command, " ");
+        else
+            strcat(command, " &\n");
+    }
+
+    // printf("DEBUG: %s", command);
+    ExecuteOnHost((UBYTE*)&command);
+}
